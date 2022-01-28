29fb58725e421ad63f716816db00e06f6536371e
Amelie Le Mestre <amelie.le.mestre@gmail.com>
2022-01-28 17:07:59 +0100

    formulaire + animation h1

--- javascript_portfolio.js
+++ javascript_portfolio.js
@@ -15,3 +15,47 @@ menu.addEventListener ('click', function (){
 }
 
 burgerMenu()
+
+
+let i = 0;
+
+var speed = 100; 
+
+function typeWriter(element,txt) {
+    console.log(txt)
+  if (i < txt.length) {
+    element.innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(function () {
+        typeWriter(element,txt);
+    }, speed);
+  }
+}
+
+
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.bottom >= 0 &&
+      rect.right >= 0 &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  scroll1 = 0
+document.addEventListener("scroll", function () {
+    if (scroll1 == 0) {
+        if (isInViewport(document.querySelector(".home__content>h1"))) {
+            typeWriter(document.querySelector(".home__content>h1"),"Amelie Le Mestre")
+            scroll1 = 1
+        }
+    }
+    if (scroll1 == 0) {
+        if (isInViewport(document.querySelector(".about-me__content>h1"))) {
+            typeWriter()
+            scroll1 = 1
+        }
+    }
+})
